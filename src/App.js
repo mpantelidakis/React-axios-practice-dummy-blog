67e394c2a421bed48e5b9e7c1794f31d3219c908
citywalk3r <minaspantelidakis@gmail.com>
2019-08-13 23:59:57 +0300

    Added basename comment in case of a real server scenario

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // Here we need to set the basename
+      // which is the path that the react app is hosted in our server
+      // <BrowserRouter basename='/my-app'>
       <BrowserRouter>
         <div className="App">
           <Blog />
